032dc1f2e7659e1d9bd3f3a2275e5635228cd0d9
Zaxuk <50300977@qq.com>
2017-09-27 15:32:30 +0800

    Update ResourceController.java

--- src/main/java/com/talkweb/ylimaf/web/controller/ResourceController.java
+++ src/main/java/com/talkweb/ylimaf/web/controller/ResourceController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,6 +16,7 @@ import java.util.List;
  * Created by ZhuXu on 2017/8/21 0021.
  */
 @RestController
+@RequestMapping(value = "/resource")
 public class ResourceController {
 
     public static final Logger LOG = LoggerFactory.getLogger(ResourceController.class);
@@ -22,12 +24,15 @@ public class ResourceController {
     @Autowired
     ResourceService resourceService;
 
-    @RequestMapping(value = "/queryResource")
-    public ModelAndView queryResource(String resourceName) {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView main() {
+        return new ModelAndView("resource");
+    }
 
+    @RequestMapping(value = "/query")
+    public Object queryResource(String resourceName) {
         List<Resource> resourceList = resourceService.queryResource(resourceName);
-
-        return new ModelAndView("resource").addObject("resourceList", resourceList);
+        return resourceList;
     }
 
 }
